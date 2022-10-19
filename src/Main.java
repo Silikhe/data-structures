88fb16d5f07ea261076e311fc848b7d9c6bd4215
silikhe <silaswans@gmail.com>
2022-10-19 22:56:04 +0300

    Add two numbers bruteforce approach

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        addTwoNumber([2,7,11,15] 9);
+        System.out.println();
     }
 }
