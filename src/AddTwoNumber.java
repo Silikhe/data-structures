4acd29b3307c90989b484cd2a2bc3f23e8a5c71e
silikhe <silaswans@gmail.com>
2022-10-24 23:22:56 +0300

    Add two numbers bruteforce approach

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class addTwoNumber {
+public class AddTwoNumber {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+//        AddTwoNumber.
 //        twoSum([2,7,11,15] 9);
         System.out.println();
     }
