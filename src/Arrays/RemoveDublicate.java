b1a6bde49ffbc1a2840a06cffe372a967dd5b3f2
silikhe <silaswans@gmail.com>
2022-10-28 02:48:01 +0300

    remove dublicate array solution

--- src/Arrays/RemoveDublicate.java
+++ src/Arrays/RemoveDublicate.java
@@ -50,8 +50,17 @@ public class RemoveDublicate {
 //        System.out.println(removeDuplicates([1,1]));
     }
 
-    public static int[] removeDuplicates(int[] nums) {
-//        System.out.println(nums);
-        return nums;
+    public int removeDuplicates(int[] nums) {
+        int insertIndex = 1;
+        for(int i = 1; i < nums.length; i++){
+            // We skip to next index if we see a duplicate element
+            if(nums[i - 1] != nums[i]) {
+                /* Storing the unique element at insertIndex index and incrementing
+                   the insertIndex by 1 */
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
     }
 }
