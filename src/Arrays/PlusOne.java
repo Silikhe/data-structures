1c956a479fef005c5091ce97d385a46f4b23db0f
silikhe <silaswans@gmail.com>
2022-11-04 23:52:46 +0300

    plus one solution 1

--- src/Arrays/PlusOne.java
+++ src/Arrays/PlusOne.java
@@ -36,5 +36,17 @@ public class PlusOne {
 //            0 <= digits[i] <= 9
 //    digits does not contain any leading 0's.
 
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
 
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
 }
