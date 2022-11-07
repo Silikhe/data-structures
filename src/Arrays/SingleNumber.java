e1df31c8cad51cf95610cc659c63223965847ff8
silikhe <silaswans@gmail.com>
2022-11-07 23:31:37 +0300

    SingleNumber solution

--- src/Arrays/SingleNumber.java
+++ src/Arrays/SingleNumber.java
@@ -29,5 +29,15 @@ public class SingleNumber {
 //    Each element in the array appears twice except for one element which appears only once.
     public int singleNumber(int[] nums) {
 
+        // do XOR operation on all element using bit wise operator concept
+
+        // for dry run take table of 1 to 9 base10 to binary
+        // XOR all element u will get unique element
+
+        int ans =0;
+        for (int i =0; i<nums.length; i++){
+            ans ^=nums[i];
+        }
+        return ans;
     }
 }
