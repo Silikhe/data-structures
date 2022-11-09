1ad7e5e6a9b82e9548d0f5baf00475c87bc6d6db
silikhe <silaswans@gmail.com>
2022-11-09 23:54:45 +0300

    Push element x onto stack.

--- src/LinkedLists/StackUsingQueues.java
+++ src/LinkedLists/StackUsingQueues.java
@@ -54,6 +54,17 @@ public class StackUsingQueues {
         top = x;
     }
 
+    public void push1(int x) {
+        q2.add(x);
+        top = x;
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
     // Removes the element on top of the stack.
     public void pop() {
         while (q1.size() > 1) {
