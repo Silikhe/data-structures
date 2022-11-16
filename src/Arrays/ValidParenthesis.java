e5228a7affc8f996466e93ce91a6acb8fbd8bd1b
silikhe <silaswans@gmail.com>
2022-11-16 23:19:19 +0300

    Valid parethesis answer

--- src/Arrays/ValidParenthesis.java
+++ src/Arrays/ValidParenthesis.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Stack;
+
 public class ValidParenthesis {
 //    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 //
@@ -28,4 +30,20 @@ public class ValidParenthesis {
 //
 //            1 <= s.length <= 104
 //    s consists of parentheses only '()[]{}'.
+
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<Character>();
+        for(char a:s.toCharArray())
+        {
+
+            //Check if stack not empty and the top element corresponds to curr char
+            if(!st.isEmpty() && (st.peek()==a-1 || st.peek()==a-2))
+                st.pop();
+                //else these migh be starting brackets or unsatisfied closing brackets
+            else
+                st.push(a);
+        }
+
+        return st.isEmpty();
+    }
 }
